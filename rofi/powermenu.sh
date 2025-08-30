5cc419d35483131f8e722851261b3ac9da43330f
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2025-08-30 17:11:11 +0530

    30-8-25

---
+++ rofi/powermenu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# List of options with emoji or icons
+options=(
+  "  Lock"
+  "  Suspend"
+  "  Logout"
+  "  Shutdown"
+  "  Reboot"
+  "  Reboot to FW"
+)
+
+# Turn array into newline-separated string
+choice=$(printf "%s\n" "${options[@]}" | rofi -dmenu -theme ~/.config/rofi/themes/powermenu-grid.rasi -p "Power Menu")
+
+case "$choice" in
+  "  Lock") swaylock ;;
+  "  Suspend") systemctl suspend ;;
+  "  Logout") hyprctl dispatch exit ;;
+  "  Shutdown") systemctl poweroff ;;
+  "  Reboot") systemctl reboot ;;
+  "  Reboot to FW") systemctl reboot --firmware-setup ;;
+esac
