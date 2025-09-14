ce3e26671227585db847bf36a948696c43331a9d
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2025-09-14 19:38:12 +0530

    14-9-25

--- rofi/list/update/update.sh
+++ rofi/list/update/update.sh
@@ -1,1 +1,16 @@
-kitty  yay -Syu
+#!/usr/bin/env bash
+
+# List of options with emoji or icons
+options=(
+  "󱧐   Update System"
+  "   Update Mirrors"
+
+)
+
+# Turn array into newline-separated string
+choice=$(printf "%s\n" "${options[@]}" | rofi -dmenu -theme ~/.config/rofi/list/update/style.rasi -p "Update")
+
+case "$choice" in
+  "󱧐   Update System") kitty sudo pacman -Syu ;;
+  "   Update Mirrors") kitty sudo reflector --age 6 --latest 20  --fastest 20 --threads 5 --sort rate --protocol https --save /etc/pacman.d/mirrorlist ;;
+esac
