d68bd960370891618525a27b4729e1b31411e1d6
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2025-09-19 01:55:45 +0530

    18-9-25

--- rofi/list/update/update.sh
+++ rofi/list/update/update.sh
@@ -3,6 +3,8 @@
 # List of options with emoji or icons
 options=(
   "󱧐   Update System"
+  "󱧐   Update AUR"
+  "󱧐   Update AUR noconfirm"
   "   Update Mirrors"
 
 )
@@ -12,5 +14,7 @@ choice=$(printf "%s\n" "${options[@]}" | rofi -dmenu -theme ~/.config/rofi/list/
 
 case "$choice" in
   "󱧐   Update System") kitty sudo pacman -Syu ;;
+  "󱧐   Update AUR") kitty yay -Syu ;;
+  "󱧐   Update AUR noconfirm") kitty yay -Suy --noconfirm ;;
   "   Update Mirrors") kitty sudo reflector --age 6 --latest 20  --fastest 20 --threads 5 --sort rate --protocol https --save /etc/pacman.d/mirrorlist ;;
 esac
